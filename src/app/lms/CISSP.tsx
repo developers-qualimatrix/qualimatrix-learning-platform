457d9177c27ebd02dcca1846458fd8fef749b8ec
vikas-narwariya <vickylodhi05@gmail.com>
2025-03-24 12:30:29 +0530

    added the consistent UI

--- src/app/lms/CISSP.tsx
+++ src/app/lms/CISSP.tsx
@@ -2,25 +2,18 @@
 
 import React, { useEffect, useState } from "react";
 import { useTheme } from "@/context/ThemeContext";
-import Image from "next/image";
-import Link from "next/link";
 import SolutionCardList from "@/components/cards/SolutionCardList";
-import TrainingCard from "@/components/cards/TrainingCard";
-import CourseAccordion from "@/components/cards/CourseAccordion";
 import { TickIcon } from "@/components/icons/SocialIcons";
 import ClientSwiper from "@/components/swiper/ClientSwiper";
 import SecurityConsulting from "@/components/cards/SecurityConsulting";
-// import { mainCardData } from "@/components/data/QualimatrixSecurityConsulting";
 import ServiceFaqAccordion from "@/components/cards/ServiceFaqAccordion";
-import KnowMoreForm from "@/components/forms/KnowMoreForm";
 import CorporateTrainingForm from "@/components/forms/CorporateTraining";
-import RegisterAccountForm from "@/components/forms/RegisterAccount";
 import ScheduleModal from "@/components/forms/ScheduleModal";
 import CourseDescriptionAccordion from "@/components/cards/CourseDescriptionAccordion";
 import TrainingCardGrid from "@/components/cards/TrainingCard";
 import SolutionCardList2 from "@/components/cards/SolutionCardList2";
 import CalendlyWidget from "@/components/cards/CalendlyWidget";
-// import CorporateTrainingForm from "@/components/forms/CorporateTrainingForm";
+import { accordionCourseDescriptionData, accordionData, cardData1, cardData2, cardData3, trainingData, whyChooseUsData } from "../constants/data";
 
 const CISSP = () => {
   const { theme } = useTheme();
@@ -36,204 +29,7 @@ const CISSP = () => {
     }
   }, [theme]);
 
-  const cardData1 = [
-    {
-      id: 1,
-      title: "Latest CISSP curriculum with regular test monitoring",
-    },
-
-    {
-      id: 2,
-      title: "Hands-on experience with 8 Domains to master Cybersecurity",
-    },
-    {
-      id: 3,
-      title: "Top certified Tutors",
-    },
-    {
-      id: 4,
-      title: "Live instructor-led training",
-    },
-  ];
-  const cardData2 = [
-    {
-      id: 1,
-      title: "Develop Customized Cloud Security Solutions.",
-    },
-    {
-      id: 2,
-      title: "Expertise in industry-leading cloud security pratices.",
-    },
-    {
-      id: 3,
-      title: "Apply advanced security frameworks and tools.",
-    },
-    {
-      id: 4,
-      title: "Experts in cost optimization for smoother operation scalability.",
-    },
-    {
-      id: 5,
-      title: "Design and function complex cloud applications.",
-    },
-    {
-      id: 6,
-      title: "Gain insights from real-world case studies.",
-    },
-  ];
-  const cardData3 = [
-    {
-      id: 1,
-      title: "Best Learning materials.",
-    },
-    {
-      id: 2,
-      title: "Mock tests and practical exams.",
-    },
-    {
-      id: 3,
-      title: "Live training sessions (16+ Hrs).",
-    },
-    {
-      id: 4,
-      title: "Assisted exam registration.",
-    },
-    {
-      id: 5,
-      title: "Digitaly accessible case studies.",
-    },
-    {
-      id: 6,
-      title: "CSA (Cloud Security Alliance) Recognized.",
-    },
-  ];
-  const accordionData = [
-    {
-      id: 1,
-      title: "Are there any group discounts for classroom training programs?",
-      description:
-        "Yes, we provide group discounts based on our set offer packages for classroom training programs. Please contact us at support@qualimatrix.tech, to get more details on discount packages.",
-    },
-    {
-      id: 2,
-      title: "Will this training session include materials?",
-      description:
-        "Yes, this training session will be coming with materials just after the training session to help students study whenever and wherever they want.   ",
-    },
-    {
-      id: 3,
-      title: "Are the practice tests necessary to be taken during the course?",
-      description:
-        "Yes, our practice tests make students ready for best results, therefore, they are officially compulsory to be taken.",
-    },
-    {
-      id: 4,
-      title: "Can this course be attended virtually/Online?",
-      description:
-        "We believe students learn better in physical classes. Though if any inconvenience, our portal gives live class option to students so they do not miss out on any learning session.",
-    },
-  ];
-
-  const accordionCourseDescriptionData = [
-    {
-      id: 1,
-      title: "Training Delivery Style",
-      content: [
-        "Our CISSP certification provides varied interactive sessions to engage, test, and analyze students’ growth and skill development. These practices typically last for 1-2 hours.",
-        "The training sessions will provide our students with insight into technical skill development. It turns participants into top-notch cybersecurity program experts. The core features of our training program include:",
-        [
-          "Hands-on Labs and Simulations",
-          "Comprehensive exam assistance and preparation",
-          "Real-world case connectivity",
-          "Interactive Discussions",
-          "Top-qualified expert-led instructions",
-          "Best Study Materials",
-        ],
-      ],
-    },
-    {
-      id: 2,
-      title: "Benefits of CISSP Training",
-      content: [
-        "Participants gain access from the crucial 8 domains of CBK (Common Body of Knowledge).",
-        "Get global recognition with official CISSP certification.",
-        "Get customized learning solutions from top-qualified tutors with lifetime access to the recorded sessions.",
-        "Advance your career options in roles like CISO (Chief Information Security Officer) or Security Manager.",
-        "Learn about ISO 27001, HIPAA, and GDPR for company regulations and compliance.",
-      ],
-    },
-    {
-      id: 3,
-      title: "Faculty",
-      content: [
-        "Our trainers are certified cybersecurity professionals, holding experience in their teaching values. They guide their students with connective teaching patterns as per each individual. From giving customized solutions to motivating them with real-world examples, they are our true heroes.",
-        "Our Faculty members include:",
-        [
-          "15+ years of expertise",
-          "Trained more than 5000 professionals",
-          "50% Trainer success records",
-        ],
-      ],
-    },
-  ];
-
-  const whyChooseUsData = [
-    {
-      id: 1,
-      imgSrc: "/lms/icon1.png",
-      title: "Accredited Courseware",
-      description:
-        "Our materials are officially qualified for top CISSP training. This helps our students excel in certification exams. Our in-depth course insights are specially designed by industry experts for the highest-quality learning.",
-    },
-    {
-      id: 2,
-      imgSrc: "/lms/icon2.png",
-      title: "Experienced Instructors",
-      description:
-        "We hire highly professional tutors in CISSP who have good training experience. They come with expert guidance, real-world examples, and in-depth insights to conquer security solutions.",
-    },
-    {
-      id: 3,
-      imgSrc: "/lms/icon3.png",
-      title: "Practical Training",
-      description:
-        "Our approach to teaching includes interactive sessions and one-to-one Q&A programs, enabling our students to be clear on every step of their learning journey.",
-    },
-    {
-      id: 4,
-      imgSrc: "/lms/icon4.png",
-      title: "99.3% Passing Rate",
-      description:
-        "  We shape a bright future for our students. Our tutors have guided 1000+ students with a structured learning path. Our students achieved 99.3% in their CISSP exam. The success rate at Qualimatrix is definite.",
-    },
-  ];
-
-  const trainingData = [
-    {
-      title: "1-on-1 Training",
-      points: [
-        "Learn at your own time",
-        "Personalized tutor assistance",
-        "Focused attention",
-      ],
-    },
-    {
-      title: "Group Training",
-      points: [
-        "Cost effective",
-        "Networking and Team bonding",
-        "Diverse Q&A sessions",
-      ],
-    },
-    {
-      title: "Corporate Training",
-      points: [
-        "Access whenever & wherever you need",
-        "Customized training",
-        "Team collaboration",
-      ],
-    },
-  ];
+  
 
   return (
     <>
